d73cb97964247ae3d946bc44149bd301947df35a
Chris Patterson <chris@phatboyg.com>
2012-03-29 19:29:25 -0500

    Upgraded version

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,7 @@ COPYRIGHT = "Copyright 2011 Chris Patterson, All rights reserved."
 include FileTest
 require 'albacore'
 
-BUILD_NUMBER_BASE = '0.4.1'
+BUILD_NUMBER_BASE = '0.4.2'
 PRODUCT = 'Automatonymous'
 CLR_TOOLS_VERSION = 'v4.0.30319'
 OUTPUT_PATH = 'bin/Release'
