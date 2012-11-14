1f756263567a3f8c813a82d0927899dc2661fca7
Chris Patterson <chris.patterson@relayhealth.com>
2012-11-14 10:22:53 -0600

    Added unit test to verify composite events fire using MassTransit events

--- src/Automatonymous/Properties/AssemblyInfo.cs
+++ src/Automatonymous/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
-
+#if DEBUG
+[assembly: InternalsVisibleTo("Automatonymous.MassTransitIntegration")]
+#else
 [assembly: InternalsVisibleTo("Automatonymous.MassTransitIntegration, PublicKey=002400000480000094000000060200000024000052534131000400000100010077a2fd2aa44b91c158fe6024be4fbd4562e3f53b2e574db07d32ee84177923babd1281ccaf1d020d125697f0ef82ea30a1e9f83fc219778f958998c6e11c547919b6c1a5eed5b01ec6fbd2ba05c77fb605bdaa068d214337ccdcfb6819449348e80a4805a478ab54a16f118136624e63197b7eda38db504b94dc0963730ce3bd")]
+#endif
