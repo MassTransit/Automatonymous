93fcf4801a99fa5a3369c8a31c5c89f7140c60f8
Chris Block <chris.block@gmail.com>
2014-10-22 00:57:24 -0500

    Split tests out to have one assert each.

--- src/Automatonymous.Tests/AutomatonymousStateMachine_Specs.cs
+++ src/Automatonymous.Tests/AutomatonymousStateMachine_Specs.cs
@@ -27,22 +27,55 @@ namespace Automatonymous.Tests
 
 			var states = stateMachine.States.ToList();
 
-			Assert.That(states, Has.Count.EqualTo(4));
-			Assert.That(states, Contains.Item(stateMachine.Initial));
-			Assert.That(states, Contains.Item(stateMachine.Final));
-			Assert.That(states, Contains.Item(stateMachine.ThisIsAState));
-			Assert.That(states, Contains.Item(stateMachine.ThisIsAlsoAState));
+			Assert.That(states, Has.Count.EqualTo(3));
+		}
+		
+		[Test]
+		public void Should_register_inherited_initial_state_property()
+		{
+			var stateMachine = new TestStateMachine();
+
+			Assert.That(stateMachine.States, Contains.Item(stateMachine.Initial));
 		}
 
 		[Test]
-		public void Should_initialize_all_state_properties()
+		public void Should_register_inherited_final_state_property()
+		{
+			var stateMachine = new TestStateMachine();
+
+			Assert.That(stateMachine.States, Contains.Item(stateMachine.Final));
+		}
+
+		[Test]
+		public void Should_register_declared_state_property()
+		{
+			var stateMachine = new TestStateMachine();
+
+			Assert.That(stateMachine.States, Contains.Item(stateMachine.ThisIsAState));
+		}
+
+		[Test]
+		public void Should_initialize_inherited_initial_state_property()
 		{
 			var stateMachine = new TestStateMachine();
 
 			Assert.That(stateMachine.Initial, Is.Not.Null);
+		}
+
+		[Test]
+		public void Should_initialize_inherited_final_state_property()
+		{
+			var stateMachine = new TestStateMachine();
+
 			Assert.That(stateMachine.Final, Is.Not.Null);
+		}
+
+		[Test]
+		public void Should_initialize_declared_state_property()
+		{
+			var stateMachine = new TestStateMachine();
+
 			Assert.That(stateMachine.ThisIsAState, Is.Not.Null);
-			Assert.That(stateMachine.ThisIsAlsoAState, Is.Not.Null);
 		}
 
 		[Test]
@@ -53,16 +86,37 @@ namespace Automatonymous.Tests
 			var events = stateMachine.Events.ToList();
 
 			Assert.That(events, Has.Count.EqualTo(2));
-			Assert.That(events, Contains.Item(stateMachine.ThisIsASimpleEvent));
-			Assert.That(events, Contains.Item(stateMachine.ThisIsAnEventConsumingData));
 		}
 
 		[Test]
-		public void Should_initialize_all_event_properties()
+		public void Should_register_simple_event_property()
+		{
+			var stateMachine = new TestStateMachine();
+
+			Assert.That(stateMachine.Events, Contains.Item(stateMachine.ThisIsASimpleEvent));
+		}
+
+		[Test]
+		public void Should_register_generic_event_property()
+		{
+			var stateMachine = new TestStateMachine();
+
+			Assert.That(stateMachine.Events, Contains.Item(stateMachine.ThisIsAnEventConsumingData));
+		}
+
+		[Test]
+		public void Should_initialize_simple_event_property()
 		{
 			var stateMachine = new TestStateMachine();
 
 			Assert.That(stateMachine.ThisIsASimpleEvent, Is.Not.Null);
+		}
+
+		[Test]
+		public void Should_initialize_generic_event_property()
+		{
+			var stateMachine = new TestStateMachine();
+
 			Assert.That(stateMachine.ThisIsAnEventConsumingData, Is.Not.Null);
 		}
 
@@ -80,7 +134,6 @@ namespace Automatonymous.Tests
 		class TestStateMachine : AutomatonymousStateMachine<Instance>
 		{
 			public State ThisIsAState { get; private set; }
-			public State ThisIsAlsoAState { get; private set; }
 			public Event ThisIsASimpleEvent { get; private set; }
 			public Event<EventData> ThisIsAnEventConsumingData { get; private set; }
 		}
