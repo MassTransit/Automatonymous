eed69366e1008a9b39eb0a9e4c17720281a998f1
Chris Patterson <chris@phatboyg.com>
2014-12-23 15:28:54 -0800

    Added internals internally, no more submodule

---
+++ src/Automatonymous/Internals/ReadOnlyProperty.cs
@@ -0,0 +1,45 @@
+// Copyright 2007-2014 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Automatonymous.Internals
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+
+    class ReadOnlyProperty<T, TProperty>
+    {
+        public readonly Func<T, TProperty> GetProperty;
+
+        public ReadOnlyProperty(PropertyInfo property)
+        {
+            Property = property;
+            GetProperty = GetGetMethod(Property);
+        }
+
+        public PropertyInfo Property { get; private set; }
+
+        public TProperty Get(T instance)
+        {
+            return GetProperty(instance);
+        }
+
+        static Func<T, TProperty> GetGetMethod(PropertyInfo property)
+        {
+            ParameterExpression instance = Expression.Parameter(typeof(T), "instance");
+            MethodCallExpression call = Expression.Call(instance, property.GetMethod);
+
+            return Expression.Lambda<Func<T, TProperty>>(call, instance).Compile();
+        }
+    }
+}
---
+++ src/Automatonymous/Internals/ReadWriteProperty.cs
@@ -0,0 +1,44 @@
+// Copyright 2007-2014 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Automatonymous.Internals
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+
+    class ReadWriteProperty<T, TProperty> :
+        ReadOnlyProperty<T, TProperty>
+    {
+        public readonly Action<T, TProperty> SetProperty;
+
+        public ReadWriteProperty(PropertyInfo property, bool includeNonPublic = false)
+            : base(property)
+        {
+            SetProperty = GetSetMethod(Property, includeNonPublic);
+        }
+
+        public void Set(T instance, TProperty value)
+        {
+            SetProperty(instance, value);
+        }
+
+        static Action<T, TProperty> GetSetMethod(PropertyInfo property, bool includeNonPublic)
+        {
+            ParameterExpression instance = Expression.Parameter(typeof(T), "instance");
+            ParameterExpression value = Expression.Parameter(typeof(TProperty), "value");
+            MethodCallExpression call = Expression.Call(instance, property.SetMethod, value);
+            return Expression.Lambda<Action<T, TProperty>>(call, new[] {instance, value}).Compile();
+        }
+    }
+}
