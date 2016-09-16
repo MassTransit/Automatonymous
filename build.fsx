253977ce655cd9a876e7b51f04289d43bd651efb
Chris Patterson <chris@phatboyg.com>
2016-09-16 10:06:47 -0700

    Updated version

--- build.fsx
+++ build.fsx
@@ -12,7 +12,7 @@ let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./Automatonymous.snk"
 
 let assemblyVersion = "3.3.0.0"
-let baseVersion = "3.3.0"
+let baseVersion = "3.5.0"
 
 let semVersion : SemVerInfo = parse baseVersion
 
