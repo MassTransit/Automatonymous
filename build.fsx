2d1f6701fec1a8627c8e4831f6a2a5314418d6b2
Chris Patterson <chris@phatboyg.com>
2016-11-07 08:56:21 -0800

    Updated GreenPipes

--- build.fsx
+++ build.fsx
@@ -12,7 +12,7 @@ let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./Automatonymous.snk"
 
 let assemblyVersion = "3.5.0.0"
-let baseVersion = "3.5.4"
+let baseVersion = "3.5.5"
 
 let semVersion : SemVerInfo = parse baseVersion
 
