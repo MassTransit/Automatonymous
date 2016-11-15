d64c25a7911faa77ccc40fa826e4c8c189e6c397
Chris Patterson <chris@phatboyg.com>
2016-11-15 07:48:30 -0800

    Updating version for pre-release

--- build.fsx
+++ build.fsx
@@ -12,7 +12,7 @@ let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./Automatonymous.snk"
 
 let assemblyVersion = "3.5.0.0"
-let baseVersion = "3.5.5"
+let baseVersion = "3.5.6"
 
 let semVersion : SemVerInfo = parse baseVersion
 
