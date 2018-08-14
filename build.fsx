7d2b2a85f8a421ffcb121d88e85a903d75677a95
Chris Patterson <chris@phatboyg.com>
2018-08-14 11:06:35 -0500

    Updated to latest GreenPipes

--- build.fsx
+++ build.fsx
@@ -9,7 +9,7 @@ let buildArtifactPath = FullName "./build_artifacts"
 let packagesPath = FullName "./src/packages"
 
 let assemblyVersion = "4.1.0.0"
-let baseVersion = "4.1.1"
+let baseVersion = "4.1.2"
 
 let envVersion = (environVarOrDefault "APPVEYOR_BUILD_VERSION" (baseVersion + ".0"))
 let buildVersion = (envVersion.Substring(0, envVersion.LastIndexOf('.')))
